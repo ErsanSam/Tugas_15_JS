4421485f1ad39d4a4c02995bfbda28b0cf3688b2
Ersan Samjaya <fernandoersan28@gmail.com>
2021-06-09 13:53:49 +0700

    Tugas 15 JS
    Tugas ini saya kumpulkan untuk NIOMIC

---
+++ index.js
@@ -0,0 +1,16 @@
+var ab = 8;
+var bc = 6;
+console.log("Nilai AB : 8");
+console.log("Nilai BC : 6");
+/*
+var x = console.log(Math.pow(8,2)) //64
+var y = console.log(Math.pow(6,2)) //36
+ x = 64;
+ y = 36;
+var z = console.log(x+y) //100
+function akar(){
+  console.log(Math.sqrt(100))
+}
+akar() //10
+*/
+console.log("panjang sisi AC pada segitiga siku-siku tersebut adalah : 10cm")
